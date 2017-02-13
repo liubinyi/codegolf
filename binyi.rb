6e7e028a4e5db1563d99e60ea867000e70dff9c1
liubinyi <bliu1@berkeley.edu>
2017-02-12 20:14:04 -0800

    shorten names

--- binyi.rb
+++ binyi.rb
@@ -2,8 +2,8 @@ v=ARGV[0]
 i=STDIN.read
 o=i.split("\n").map{|z|z.split("")}
 t=o.transpose
-0.upto(6){|col|rowindex=5-t[col].reverse.index{|e|e=~/\W+/}.to_i
+0.upto(6){|c|r=5-t[c].reverse.index{|e|e=~/\W+/}.to_i
 x=i.split("\n").map{|e|e.dup}
-x[rowindex][col]=v if x[rowindex][col]=='.'
+x[r][c]=v if x[r][c]=='.'
 w=x.join("\n")
-puts col+1 if !w.match(/(?:([xo])(?=\1\1\1))/mx).nil?||!w.match(/(?:([xo])(?=.{7}\1.{7}\1.{7}\1))/mx).nil?||!w.match(/(?:([xo])(?=.{6}\1.{6}\1.{6}\1))/mx).nil?||!w.match(/(?:([xo])(?=.{8}\1.{8}\1.{8}\1))/mx).nil?}
+puts c+1 if !w.match(/(?:([xo])(?=\1\1\1))/mx).nil?||!w.match(/(?:([xo])(?=.{7}\1.{7}\1.{7}\1))/mx).nil?||!w.match(/(?:([xo])(?=.{6}\1.{6}\1.{6}\1))/mx).nil?||!w.match(/(?:([xo])(?=.{8}\1.{8}\1.{8}\1))/mx).nil?}
